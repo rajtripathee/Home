b34117e2eb83fb1763682f7e218dd4fe0a4101a7
rajtripathee <raj@rajk.com.np>
2018-08-21 22:13:27 -0700

    Update SHT31D.cpp

--- SHT31D.cpp
+++ SHT31D.cpp
@@ -1,5 +1,28 @@
-#include SHT31D.h
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include "SHT31D.h"
 
+int SHT31D::begin(int i2c_address, uint8_t sht31_address){
+ char filename[20];
+ int fp;
+
+ snprintf(filename, 19, "/dev/i2c-%d", i2c_address);
+ fp = open(filename, O_RDWR);
+ if (fp < 0) {
+  return fp;
+ }
+
+ if (ioctl(fp, I2C_SLAVE, sht31_address) < 0) {
+  close(fp);
+  return -1;
+  }
+  
+	return fp;
+}
+	
+int SHT31D::close(int fp){
+  return close(fp);
+}
 uint8_t SHT31D::crc8(const uint8_t *data, int len)
 {
 /*
