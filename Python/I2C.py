6bb6cf0d8e7031e5c1aa70673072fe1468d1d87d
Raj Tripathee <Tripathee@Nepal.local>
2018-10-13 23:00:29 -0700

    I2C and platform library for PI

---
+++ Python/I2C.py
@@ -0,0 +1,86 @@
+#
+# TO DO: Add support for the little endian / big endian data
+#
+
+import pi_platform as Platform
+
+
+def default_bus():
+    plat = Platform.detect_platform()
+    if plat == Platform.PI:
+        if Platform.revision() == 1.0:
+            return 0
+        else:
+            # busnum is 1 for Raspberry Pi B and above.
+            return 1
+    else:
+        raise RuntimeError ("I2c Bus for the platform could not be detected.")
+
+def i2c_device(address, busnum=None, i2c_interface=None, **kwargs):
+    if busnum == None:
+        busnum = default_bus()
+    return Device(address, busnum, i2c_interface, **kwargs)  
+
+class Device(object):
+    def __init__(self, address, busnum, i2cinterface=None):
+        self._address = address
+        if i2cinterface is None:
+            import smbus
+            self._bus = smbus.SMBus(busnum)
+        else:
+            self._bus = i2cinterface(busnum)
+    
+    def writeRaw8bit(self,data):
+        # Write 8-bit data on the bus.
+        data = data & 0xFF
+        self._bus.write_byte(self._address, data)
+
+    def write8bit(self,register,data):
+        # Write 8-bit data to the specified register.
+        data = data & 0xFF
+        self._bus.write_byte(self._address, register, data)
+
+    def write16bit(self,register,data):
+        # Write 16-bit data to the specified register.
+        data = data & 0xFFFF
+        self._bus.write_word_data(self._address, register, data)
+    
+    def writeBlock(self,register, data):
+        # Write block of data to the specified register.
+        self._bus.write_i2c_block_data(self._address, register, data)
+    
+    def readRaw8bit(self):
+        # Read 8-bit data on the bus.
+        data = self._bus.read_byte(self._address) & 0xFF
+        return data
+    
+    def readU8bit(self, register):
+        # Read unsigned  8-bit from the specified register.
+        data = self._bus.read_byte_data(self._address, register) & 0xFF
+        return data
+    
+    def readS8bit(self, register):
+        # Read signed 8-bit from the specified register.
+        data = self.readU8bit(register)
+        if data > 127:
+            data -= 256
+        return data
+    
+    def readU16bit(self, register, little_endian = True):
+        # Read Un
+        data = self._bus.read_word_data(self._address, register) & 0xFFFF
+        if not little_endina:
+            data = ((data <<8) & 0xFF00) + (data >> 8)
+        return data
+
+    def readS16bit(self, register, little_endian=True):
+        # Read signed 16-bit data from the specified register on the specified endianesss.
+        data = self.readU16bit(register, little_endian)
+        if data > 32767:
+            data -= 65536
+        return data
+
+    def readBlock(self, register, length):
+        # Read block of data of length from the register
+        data = self._bus.read_i2c_block_data(self._address, register, length)
+        return data
---
+++ Python/pi_platform.py
@@ -0,0 +1,48 @@
+#
+# TO DO: Detect Different RaspberryPi Platform
+#
+
+import re
+
+def version():
+    with open("/proc/cpuinfo", "r") as infile:
+        cpuinfo = infile.read()
+    
+    match = re.search("^Hardware\s+:\s+(\w+)$", cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
+
+    if not match:
+        return None
+    if match.group(1) == "BCM2835":
+        return 1
+    if match.group(1) == "BCM2836":
+        return 2
+    if match.group(1) == "BCM2837":
+        return 3
+    else:
+        return None
+ 
+def revision():
+    with open("/proc/cpuinfo", "r") as infile:
+        for line in infile:
+            match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
+            if match and match.group(1) in ['0002', '0003', '0010', '0011', '0014', '1040', '20a0']:
+                return 1.0
+            if match and match.group(1) in ['0012', '0015', '1041', '0021', '00c1']:
+                return 1.1
+            if match and match.group(1) in ['0013', '2042', '0032','0092', '2082']:
+                return 1.2
+            if match and match.group(1) in ['0093', '20d3']:
+                return 1.3
+            elif match:
+                return 2.0
+    raise RuntimeError("PI revision could not be determined.")
+
+
+def detect_platform():
+    pi = pi_version()
+    if pi is not None:
+        return PI
+    
+    # Returns UNKNOWN if platform could not be determined.
+    return UNKNOWN
+    
