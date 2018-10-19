ff6818d33c9c65d89c47550f1f224c8c528e3f02
Raj Tripathee <Tripathee@Nepal.local>
2018-10-19 00:22:14 -0700

    Upload the average of 1 Minute sensor data to Firebase

--- Lib/SHT31_Example1.py
+++ Lib/SHT31_Example1.py
@@ -1,63 +1,52 @@
-# from Adafruit_SHT31 import *
 from time import sleep
 from SHT31D import *
 from firebase import firebase
 
 import jason
+import time
+import datetime
 #logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
 sensor = SHT31D(address = 0x44)
-#sensor = SHT31(address = 0x45)
 
-# def print_status():
-#     status = sensor.read_status()
-#     is_data_crc_error = sensor.is_data_crc_error()
-#     is_command_error = sensor.is_command_error()
-#     is_reset_detected = sensor.is_reset_detected()
-#     is_tracking_temperature_alert = sensor.is_tracking_temperature_alert()
-#     is_tracking_humidity_alert = sensor.is_tracking_humidity_alert()
-#     is_heater_active = sensor.is_heater_active()
-#     is_alert_pending = sensor.is_alert_pending()
-#     print 'Status           = {:04X}'.format(status)
-#     print '  Data CRC Error = {}'.format(is_data_crc_error)
-#     print '  Command Error  = {}'.format(is_command_error)
-#     print '  Reset Detected = {}'.format(is_reset_detected)
-#     print '  Tracking Temp  = {}'.format(is_tracking_temperature_alert)
-#     print '  Tracking RH    = {}'.format(is_tracking_humidity_alert)
-#     print '  Heater Active  = {}'.format(is_heater_active)
-#     print '  Alert Pending  = {}'.format(is_alert_pending)
+firebase = firebase.FirebaseApplication("https://smartghar-bac56.firebaseio.com/", None)
 
-# loop = 0
-firebase = firebase.FirebaseApplication("https://smartghar-bac56.firebaseio.com/, None)
+def firebase_update(degree, humidity):
+    thisTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def firebase_update():
-    degree = sensor.temperature
-    humidity = sensor.humidity
-
-    if degree is not None and humidity is not none:
-        sleep(5)
-        temperature = '{0:0.02f} deg C'.format(degree)
+    if degree is not None and humidity is not None:
+        temperature = '{0:0.2f} deg C'.format(degree)
         humid =  '{0:0.2f} %'.format(humidity)
     else:
-		print('Failed to get reading. Try again!')	
-		sleep(10)
+        print('Failed to get reading. Try again!')	
+        sleep(10)
     
-    data = {"temp": temperature, "humidity": humidity}
-    firebase.post('/TempHumid', data)
+    # Update the average of 1 minute data to firebase
+    data = { "Humid": humidity,"Temp": temperature, ,"Time":thisTime }
+    firebase.post('/Room1/SHT31D', data)
 
-print("Temperature      Humidity")
+def average_temphumid():
+    degree = sensor.temperature()
+    humidity = sensor.humidity()
+
+    # Get the average temperature and humidity in a minute
+    total_temp, total_humid = 0, 0
+    for i in range(60):
+        total_temp += degree
+        total_humid += humidity
+        sleep(1)
+    
+    average_temp = total_temp / 60
+    average_humid = total_humid / 60
+
+    firebase_update(average_temp, average_humid)
+
 while True:
-    firebase_update()
-    # degrees = sensor.temperature()
-    # humidity = sensor.humidity()
+
+    average_temphumid()
     # print_status()
     # print ('{0:0.3f} deg C      {1:0.2f} %'.format(degrees,humidity))
     # print ('{0:0.2f} %'.format(humidity))
-    # sensor.clear_status()
-    # sensor.set_heater(True)
-    # print_status()
-    # sensor.set_heater(False)
-    # print_status()
     # loop += 1
     # sleep(4)
     # if loop == 10:
